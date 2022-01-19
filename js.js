70f94369a3c15ade5cff2e07c9ad614fc8a1cec5
Ana Ramos <54245233+Anaarmos@users.noreply.github.com>
2022-01-19 11:43:47 -0300

    Projeto final para a entrega

---
+++ js.js
@@ -0,0 +1,87 @@
+function boitataUm(){ 	
+var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+if (escolher == "1") { 
+alert('Ótima escolha! Vamos para a próxima.'); 
+location.replace("boitataDois.html"); 
+}else if(escolher=="2"){ 
+alert('Ops!'); 
+location.replace("perdeu.html") 
+}} 
+		
+function boitataDois(){ 
+var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+if (escolher == "2") { 
+alert('Ótima escolha! Vamos para a próxima.'); 
+location.replace("boitataTres.html"); 
+}else if(escolher=="1"){ 
+alert('Ops!'); 
+location.replace("perdeu.html") 
+}} 
+		
+function boitataTres(){ 
+var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+
+if (escolher == "2") {alert('Você foi incrível! Parabéns por ajudar nosso héroi a combater os madeireiros.'); 
+location.replace("ganhou.html"); 
+}
+
+else if(escolher=="1"|escolher=="3"){alert('Ops!'); 
+location.replace("perdeu.html") 
+}} 
+		
+function iaraUm(){ 
+var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?');
+
+if (escolher == "1") {alert('Ótima escolha! Vamos para a próxima.'); 
+location.replace("iaraDois.html"); 
+}
+
+else if(escolher=="2"){alert('Ops!'); 
+location.replace("perdeu.html") 
+}} 
+		
+function iaraDois(){ 
+		var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+		if (escolher == "2") { 
+		alert('Ótima escolha! Vamos para a próxima.'); 
+		location.replace("iaraTres.html"); 
+		}else if(escolher=="1"){ 
+		alert('Ops!'); 
+		location.replace("perdeu.html") 
+		}} 
+		function iaraTres(){ 
+		var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+		if (escolher == "1") { 
+		alert('Você foi incrível! Parabéns por ajudar nosso héroi a combater os madeireiros.'); 
+		location.replace("ganhou.html"); 
+		}else if(escolher=="2"|escolher=="3"){ 
+		alert('Ops!'); 
+		location.replace("perdeu.html") 
+		}} 
+		function caiporaUm(){ 
+		var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+		if (escolher == "1") { 
+		alert('Ótima escolha! Vamos para a próxima.'); 
+		location.replace("caiporaDois.html"); 
+		}else if(escolher=="2"){ 
+		alert('Ops!'); 
+		location.replace("perdeu.html") 
+		}} 
+		function caiporaDois(){ 
+		var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+		if (escolher == "2") { 
+		alert('Ótima escolha! Vamos para a próxima.'); 
+		location.replace("caiporaTres.html"); 
+		}else if(escolher=="1"){ 
+		alert('Ops!'); 
+		location.replace("perdeu.html") 
+		}} 
+		function caiporaTres(){ 
+		var escolher = prompt('Agora é com você! Escolha sua opção: 1, 2 ou 3?'); 
+		if (escolher == "3") { 
+		alert('Você foi incrível! Parabéns por ajudar nosso héroi a combater os madeireiros.'); 
+		location.replace("ganhou.html"); 
+		}else if(escolher=="1"|escolher=="2"){ 
+		alert('Ops!'); 
+		location.replace("perdeu.html") 
+		}}
